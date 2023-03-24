37562b38451d4af48c2c38e71dc4a49b861da182
Erdem BAYIR <128245457+erdembayir@users.noreply.github.com>
2023-03-24 13:38:48 +0300

    Update multiplicationproject.js

--- multiplicationproject.js
+++ multiplicationproject.js
@@ -1,5 +1,12 @@
-const multiplication = (...args) => {
+const calculateArguments = (...args) => {
     const numberArray = args.filter (x=> typeof x === "number");
-    return numberArray.reduce((acc, val) => acc* val);
-}
-console.log('ÇARPMA', multiplication(args = 5,4, "hele"));
+ 
+    if (typeof numberArray === "number") {
+       return numberArray.reduce((acc, val) => acc* val);  
+    } else {
+       console.log("Lütfen Yalnızca Sayı Giriniz.");
+       
+    }
+ }
+ 
+ console.log('Carpim', calculateArguments(8,4,1, "erdem"))
